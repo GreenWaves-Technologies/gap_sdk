aac8efc5bb4da17ae5f51d39e724f299873ba4a9
Yao ZHANG <yao.zhang@greenwaves-technologies.com>
2018-10-08 17:17:55 +0200

    added some env parameters

--- sourceme.sh
+++ sourceme.sh
@@ -6,9 +6,10 @@ export TARGET_INSTALL_DIR=$GAP_SDK_HOME/install/gap
 export BUILD_DIR=$GAP_SDK_HOME/build
 export INSTALL_DIR=$GAP_SDK_HOME/install/workstation
 export DEP_DIRS=$INSTALL_DIR
-export LIB_DIR=$TARGET_INSTALL_DIR/libs
 
 # For pulp os
+export PULP_LIB_DIR=$TARGET_INSTALL_DIR/libs
+export PULP_INC_DIR=$TARGET_INSTALL_DIR/include
 export RUNTIME_PATH=$GAP_SDK_HOME/pulp-os
 # For mbed os
 export MBED_PATH=$GAP_SDK_HOME
