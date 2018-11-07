4009b8dd3dedb2645cb51ea0f1fb6967d55b8a36
Yao ZHANG <yao.zhang@greenwaves-technologies.com>
2018-11-07 17:21:00 +0100

    updated some user script to match the bridge changes

--- examples/mbed-examples/test_application/test_KWS/i2s.py
+++ examples/mbed-examples/test_application/test_KWS/i2s.py
@@ -4,36 +4,56 @@ import sys
 import time
 
 def i2s(bridge):
-    print ("i2s.py execution on-going")
-    addrWavOutHead = os.popen("riscv32-unknown-elf-nm test |grep -w 'i2s_out_header$'").read()
-    addrWavOutHead = (addrWavOutHead.split())[0]
 
-    while (1) :
-        time.sleep(1)
-        input("Press return to start sound acquisition:")
-        recReady = int(("0x"+addrWavOutHead), 16) + 8
-        bridge.write_32(recReady, 0)
+    class state:
+        addrWavOutHead = 0
+        initialized = False
+        started = False
+        recReady = False
 
-        micReady = int(("0x"+addrWavOutHead), 16) + 12
-        bridge.write_32(micReady, 1)
+    def init_state():
+        state.started = False
+        state.recReady = False
 
-        recReady = int(("0x"+addrWavOutHead), 16) + 8
-        WavOutFlag = bridge.read_32(recReady)
-        while ( int(WavOutFlag) == 0 ):
-            time.sleep(1)
-            recReady = int(("0x"+addrWavOutHead), 16) + 8
+    def do_i2s():
+        if not state.initialized:
+            print ("i2s.py execution on-going")
+            addrHead = os.popen("riscv32-unknown-elf-nm test |grep -w 'i2s_out_header$'").read()
+            state.addrWavOutHead = (addrHead.split())[0]
+            state.initialized = True
+            return 3000000
+
+        if not state.started:
+            input("Press return to start sound acquisition:")
+            micReady = int(("0x"+state.addrWavOutHead), 16) + 12
+            bridge.write_32(micReady, 1)
+            state.started = True
+            return 0
+
+        if not state.recReady:
+            recReady = int(("0x"+state.addrWavOutHead), 16) + 8
             WavOutFlag = bridge.read_32(recReady)
+            if (int(WavOutFlag) == 0):
+                return 1000000
+            else:
+                l2Addr = int(("0x"+state.addrWavOutHead), 16)
+                addrWav = bridge.read_32(l2Addr)
+                size = int(("0x"+state.addrWavOutHead), 16) + 4
+                WavSize = bridge.read_32(size)
+                with open("wavOut.wav", 'wb') as waveOut:
+                    print ("py: now download", WavSize, "bytes")
+                    for byte in bridge.read(addrWav, WavSize):
+                        waveOut.write(byte)
 
-        l2Addr = int(("0x"+addrWavOutHead), 16)
-        addrWav = bridge.read_32(l2Addr)
-        size = int(("0x"+addrWavOutHead), 16) + 4
-        WavSize = bridge.read_32(size)
+                waveOut.close()
+                recReady = int(("0x"+state.addrWavOutHead), 16) + 8
+                bridge.write_32(recReady, 0)
+                os.system("if [ ! -f ../../../waveOut.wav ]; then ln -s BUILD/GAP8/GCC_RISCV/wavOut.wav ../../../wavOut.wav 1>/dev/null 2>/dev/null; fi")
 
-#        with open("wavOut.wav", 'wb') as waveOut:
-#            print ("py: now download", WavSize, "bytes")
-#            for byte in bridge.read(addrWav, WavSize):
-#                waveOut.write(byte)
-#        waveOut.closed
+                print(".wav have been saved")
+                init_state()
+                return 1000000
 
-        os.system("if [ ! -f ../../../waveOut.wav ]; then ln -s BUILD/GAP8/GCC_RISCV/wavOut.wav ../../../wavOut.wav 1>/dev/null 2>/dev/null; fi")
-        time.sleep(1)
+    bridge.script_cb = bridge.timer_cb_typ(do_i2s)
+    bridge.module.bridge_loop_timeout(bridge.script_cb, 100000)
+
--- examples/pulp-examples/periph/i2s/i2s.py
+++ examples/pulp-examples/periph/i2s/i2s.py
@@ -1,75 +1,60 @@
 #!/usr/bin/python
-
-# Copyright (c) 2017 GreenWaves Technologies SAS
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of GreenWaves Technologies SAS nor the names of its
-#    contributors may be used to endorse or promote products derived from
-#    this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
-
 import os
 import sys
 import time
 
 def i2s(bridge):
-    addrWavOutHead = os.popen("riscv32-unknown-elf-nm test |grep -w 'I2sOutHeader$'").read()
-    addrWavOutHead = (addrWavOutHead.split())[0]
 
-    cStarted = int(("0x"+addrWavOutHead), 16) + 16
-    st = bridge.read_32(cStarted)
-    while ( int(st) == 0 ):
-        print ("py: wait c code to start")
-        time.sleep(1)
-        cStarted = int(("0x"+addrWavOutHead), 16) + 16
-        st = bridge.read_32(cStarted)
+    class state:
+        addrWavOutHead = 0
+        initialized = False
+        started = False
+        recReady = False
 
-    input("py: Press return to start sound acquisition:")
+    def init_state():
+        state.started = False
+        state.recReady = False
 
-    micReady = int(("0x"+addrWavOutHead), 16) + 12
-    bridge.write_32(micReady, 1)
+    def do_i2s():
+        if not state.initialized:
+            print ("i2s.py execution on-going")
+            addrHead = os.popen("riscv32-unknown-elf-nm test |grep -w 'I2sOutHeader$'").read()
+            state.addrWavOutHead = (addrHead.split())[0]
+            state.initialized = True
+            return 3000000
 
-    recReady = int(("0x"+addrWavOutHead), 16) + 8
-    WavOutFlag = bridge.read_32(recReady)
-    while ( int(WavOutFlag) == 0 ):
-        time.sleep(1)
-        recReady = int(("0x"+addrWavOutHead), 16) + 8
-        WavOutFlag = bridge.read_32(recReady)
-        print ("py: waiting recReady")
+        if not state.started:
+            input("Press return to start sound acquisition:")
+            micReady = int(("0x"+state.addrWavOutHead), 16) + 12
+            bridge.write_32(micReady, 1)
+            state.started = True
+            return 0
 
-    l2Addr = int(("0x"+addrWavOutHead), 16)
-    addrWav = bridge.read_32(l2Addr)
-    size = int(("0x"+addrWavOutHead), 16) + 4
-    WavSize = bridge.read_32(size)
-    with open("wavOut.wav", 'wb') as waveOut:
-        print ("py: now download", WavSize, "bytes")
-        for byte in bridge.read(addrWav, WavSize):
-            waveOut.write(byte)
+        if not state.recReady:
+            recReady = int(("0x"+state.addrWavOutHead), 16) + 8
+            WavOutFlag = bridge.read_32(recReady)
+            if (int(WavOutFlag) == 0):
+                print ("py: waiting recReady ")
+                return 1000000
+            else:
+                l2Addr = int(("0x"+state.addrWavOutHead), 16)
+                addrWav = bridge.read_32(l2Addr)
+                size = int(("0x"+state.addrWavOutHead), 16) + 4
+                WavSize = bridge.read_32(size)
+                with open("wavOut.wav", 'wb') as waveOut:
+                    print ("py: now download", WavSize, "bytes")
+                    for byte in bridge.read(addrWav, WavSize):
+                        waveOut.write(byte)
 
-    waveOut.closed
+                waveOut.close()
+                recReady = int(("0x"+state.addrWavOutHead), 16) + 8
+                bridge.write_32(recReady, 0)
+                os.system("if [ ! -f ../../../waveOut.wav ]; then ln -s BUILD/GAP8/GCC_RISCV/wavOut.wav ../../../wavOut.wav 1>/dev/null 2>/dev/null; fi")
 
-    recReady = int(("0x"+addrWavOutHead), 16) + 8
-    bridge.write_32(recReady, 0)
+                print(".wav have been saved")
+                init_state()
+                return 1000000
 
-    os.system("if [ ! -f ../../../waveOut.wav ]; then ln -s BUILD/GAP8/GCC_RISCV/wavOut.wav ../../../wavOut.wav 1>/dev/null 2>/dev/null; fi")
-    os.system("vlc wavOut.wav 1>/dev/null 2>/dev/null &")
+    bridge.script_cb = bridge.timer_cb_typ(do_i2s)
+    bridge.module.bridge_loop_timeout(bridge.script_cb, 100000)
+
