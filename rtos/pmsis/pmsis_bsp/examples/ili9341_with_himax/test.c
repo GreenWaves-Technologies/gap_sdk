872018a8a8645e241bfb62b050e20910e4c08539
Igor Murzov <igor.murzov@xperience.ai>
2019-11-15 18:50:43 +0300

    Fix camera-related applications build
    Drop unused CHUNK_NUM macro

--- rtos/pmsis/pmsis_bsp/examples/ili9341_with_himax/test.c
+++ rtos/pmsis/pmsis_bsp/examples/ili9341_with_himax/test.c
@@ -44,12 +44,12 @@ static void cam_handler(void *arg);
 
 static void cam_handler(void *arg)
 {
-  camera_control(&device, CAMERA_CMD_STOP, 0);
+  pi_camera_control(&device, PI_CAMERA_CMD_STOP, 0);
 
 #ifdef USE_BRIDGE
   rt_bridge_fb_update(fb, (unsigned int)imgBuff0, 0, 0, CAM_WIDTH, CAM_HEIGHT, NULL);
-  camera_capture_async(&device, imgBuff0, CAM_WIDTH*CAM_HEIGHT, pi_task_callback(&task, cam_handler, NULL));
-  camera_control(&device, CAMERA_CMD_START, 0);
+  pi_camera_capture_async(&device, imgBuff0, CAM_WIDTH*CAM_HEIGHT, pi_task_callback(&task, cam_handler, NULL));
+  pi_camera_control(&device, PI_CAMERA_CMD_START, 0);
 
 #else
   #if 1
@@ -64,8 +64,8 @@ static void cam_handler(void *arg)
 
 static void lcd_handler(void *arg)
 {
-  camera_control(&device, CAMERA_CMD_START, 0);
-  camera_capture_async(&device, imgBuff0, CAM_WIDTH*CAM_HEIGHT, pi_task_callback(&task, cam_handler, NULL));
+  pi_camera_control(&device, PI_CAMERA_CMD_START, 0);
+  pi_camera_capture_async(&device, imgBuff0, CAM_WIDTH*CAM_HEIGHT, pi_task_callback(&task, cam_handler, NULL));
 
 }
 
@@ -86,13 +86,13 @@ static int open_bridge()
 static int open_display(struct pi_device *device)
 {
 #ifndef USE_BRIDGE
-  struct ili9341_conf ili_conf;
+  struct pi_ili9341_conf ili_conf;
 
-  ili9341_conf_init(&ili_conf);
+  pi_ili9341_conf_init(&ili_conf);
 
   pi_open_from_conf(device, &ili_conf);
 
-  if (display_open(device))
+  if (pi_display_open(device))
     return -1;
 
   //if (display_ioctl(device, ILI_IOCTL_ORIENTATION, ILI_ORIENTATION_270))
@@ -112,16 +112,16 @@ static int open_display(struct pi_device *device)
 
 static int open_camera_himax(struct pi_device *device)
 {
-  struct himax_conf cam_conf;
+  struct pi_himax_conf cam_conf;
 
-  himax_conf_init(&cam_conf);
+  pi_himax_conf_init(&cam_conf);
 
 #ifdef QVGA
-  cam_conf.format = CAMERA_QVGA;
+  cam_conf.format = PI_CAMERA_QVGA;
 #endif
 
   pi_open_from_conf(device, &cam_conf);
-  if (camera_open(device))
+  if (pi_camera_open(device))
     return -1;
 
   return 0;
@@ -135,25 +135,25 @@ static int open_camera_himax(struct pi_device *device)
 
 static int open_camera_mt9v034(struct pi_device *device)
 {
-  struct mt9v034_conf cam_conf;
+  struct pi_mt9v034_conf cam_conf;
 
-  mt9v034_conf_init(&cam_conf);
+  pi_mt9v034_conf_init(&cam_conf);
 
 #ifdef QVGA
-  cam_conf.format = CAMERA_QVGA;
+  cam_conf.format = PI_CAMERA_QVGA;
 #endif
 #ifdef QQVGA
   cam_conf.format = CAMERA_QQVGA;
 #endif
 
   pi_open_from_conf(device, &cam_conf);
-  if (camera_open(device))
+  if (pi_camera_open(device))
     return -1;
 
   uint16_t val = MT9V034_BLACK_LEVEL_AUTO;
-  camera_reg_set(device, MT9V034_BLACK_LEVEL_CTRL, (uint8_t *) &val);
+  pi_camera_reg_set(device, MT9V034_BLACK_LEVEL_CTRL, (uint8_t *) &val);
   val = MT9V034_AEC_ENABLE_A|MT9V034_AGC_ENABLE_A;
-  camera_reg_set(device, MT9V034_AEC_AGC_ENABLE, (uint8_t *) &val);
+  pi_camera_reg_set(device, MT9V034_AEC_AGC_ENABLE, (uint8_t *) &val);
 
 
   return 0;
@@ -214,16 +214,16 @@ void test_ili9341_with_himax(void)
   while (1)
   {
       #if (ASYNC)
-      camera_control(&device, CAMERA_CMD_STOP, 0);
-      camera_capture_async(&device, imgBuff0, CAM_WIDTH*CAM_HEIGHT, pi_task_callback(&task, cam_handler, &device));
-      camera_control(&device, CAMERA_CMD_START, 0);
+      pi_camera_control(&device, PI_CAMERA_CMD_STOP, 0);
+      pi_camera_capture_async(&device, imgBuff0, CAM_WIDTH*CAM_HEIGHT, pi_task_callback(&task, cam_handler, &device));
+      pi_camera_control(&device, PI_CAMERA_CMD_START, 0);
       pi_task_wait_on(&task);
       #else
       printf("Camera start.\n");
-      camera_control(&device, CAMERA_CMD_START, 0);
-      camera_capture(&device, imgBuff0, CAM_WIDTH*CAM_HEIGHT);
+      pi_camera_control(&device, PI_CAMERA_CMD_START, 0);
+      pi_camera_capture(&device, imgBuff0, CAM_WIDTH*CAM_HEIGHT);
       printf("Camera image captured.\n");
-      camera_control(&device, CAMERA_CMD_STOP, 0);
+      pi_camera_control(&device, PI_CAMERA_CMD_STOP, 0);
       printf("Camera stop.\n");
       //pmsis_exit(0);
       #endif
