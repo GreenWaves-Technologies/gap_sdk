8808ee9a5e5678e61f26266150b9efc92346226a
Gregor Bader <gregor.bader@microtronics.com>
2019-11-28 13:30:57 +0100

    fixed configs/gapoc_a_v2.sh

--- configs/gapoc_a_v2.sh
+++ configs/gapoc_a_v2.sh
@@ -5,8 +5,8 @@ export GAP_SDK_HOME="$(dirname $(dirname "$(readlink -f "${BASH_SOURCE[0]}")"))"
 export TARGET_CHIP_FAMILY="GAP8"
 export TARGET_CHIP="GAP8_V2"
 export TARGET_NAME="gap_rev1"
-export PULP_CURRENT_CONFIG=$BOARD_NAME@config_file=config/$BOARD_NAME.json
 export BOARD_NAME=gapoc_a
+export PULP_CURRENT_CONFIG=$BOARD_NAME@config_file=config/$BOARD_NAME.json
 export GVSOC_CONFIG=$BOARD_NAME
 
 source $GAP_SDK_HOME/configs/common.sh
