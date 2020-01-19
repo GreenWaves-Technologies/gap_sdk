439c4edcd1e1ec04389eb96d024edc089cb97829
Francesco Paci <francesco.paci@greenwaves-technologies.com>
2020-01-19 11:16:52 +0100

    uniformed FC_L1_MEM

--- rtos/pulp/pulp-os/include/rt/rt_data.h
+++ rtos/pulp/pulp-os/include/rt/rt_data.h
@@ -141,6 +141,7 @@ typedef void (*rt_error_callback_t)(void *arg, rt_event_t *event, int error, voi
 #define RT_FC_GLOBAL_DATA __attribute__((section(".data_fc")))
 
 #define RT_FC_SHARED_DATA __attribute__((section(".data_fc")))
+#define FC_L1_MEM RT_FC_GLOBAL_DATA
 
 #define RT_L2_DATA __attribute__((section(".l2_data")))
 #define L2_DATA RT_L2_DATA
