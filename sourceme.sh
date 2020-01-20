546de2f07d99f23b4b366019717fd624f16b86af
S. Sivarajah <sivarajah.sivapiriyan@greenwaves-technologies.com>
2020-01-20 11:46:52 +0100

    Update script sourceme.sh : + Explicit names for boards and chips + Add gapoc_b

--- sourceme.sh
+++ sourceme.sh
@@ -1,12 +1,36 @@
 #! /bin/bash
 
-export GAP_SDK_HOME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-
-read -p "Which chip you want to use: 1-GAP8 2-GAP8_V2 [1]: " chip
-chip=${chip:-1}
-if [ $chip == "1"  ]; then
-    source $GAP_SDK_HOME/configs/gap8.sh
-elif [ $chip == "2"  ]; then
-    source $GAP_SDK_HOME/configs/gap8_v2.sh
+if [  -n "${ZSH_VERSION:-}" ]; then 
+	DIR="$(readlink -f -- "${(%):-%N}")"
+	DIRNAME="$(dirname $DIR)"
+	GAP_SDK_HOME=$(echo $DIRNAME)
+	export GAP_SDK_HOME
+else
+	export GAP_SDK_HOME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 fi
-echo The target chip you have chosen is : $TARGET_CHIP
+
+echo "Which board you want to use:
+     1-GAPUINO
+     2-GAPUINO_V2
+     3-GAPOC_A
+     4-GAPOC_A_V2
+     5-GAPOC_B
+     6-GAPOC_B_V2 [1]: "
+read chip
+chip=${chip:-1}
+if [ $chip -eq 1  ]; then
+    source $GAP_SDK_HOME/configs/gapuino.sh
+elif [ $chip -eq 2  ]; then
+    source $GAP_SDK_HOME/configs/gapuino_v2.sh
+elif [ $chip -eq 3  ]; then
+    source $GAP_SDK_HOME/configs/gapoc_a.sh
+elif [ $chip -eq 4  ]; then
+    source $GAP_SDK_HOME/configs/gapoc_a_v2.sh
+elif [ $chip -eq 5  ]; then
+    source $GAP_SDK_HOME/configs/gapoc_b.sh
+elif [ $chip -eq 6  ]; then
+    source $GAP_SDK_HOME/configs/gapoc_b_v2.sh
+else
+	echo "Unknown board: $chip"
+fi
+echo The target board you have chosen is : $BOARD_NAME
