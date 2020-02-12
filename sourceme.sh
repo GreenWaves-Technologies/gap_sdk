97e5ae57697b4e4a673cb07878395603a9bce1db
S. Sivarajah <sivarajah.sivapiriyan@greenwaves-technologies.com>
2020-02-12 09:48:52 +0100

    Update sourceme to propose available boards

--- sourceme.sh
+++ sourceme.sh
@@ -1,36 +1,43 @@
 #! /bin/bash
 
-if [  -n "${ZSH_VERSION:-}" ]; then 
-	DIR="$(readlink -f -- "${(%):-%N}")"
-	DIRNAME="$(dirname $DIR)"
-	GAP_SDK_HOME=$(echo $DIRNAME)
-	export GAP_SDK_HOME
+if [ -n "${ZSH_VERSION:-}" ]
+then
+    DIR="$(readlink -f -- "${(%):-%N}")"
+    DIRNAME="$(dirname $DIR)"
+    GAP_SDK_HOME=$(echo $DIRNAME)
+    export GAP_SDK_HOME
 else
-	export GAP_SDK_HOME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+    export GAP_SDK_HOME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 fi
 
-echo "Which board you want to use:
-     1-GAPUINO
-     2-GAPUINO_V2
-     3-GAPOC_A
-     4-GAPOC_A_V2
-     5-GAPOC_B
-     6-GAPOC_B_V2 [1]: "
-read chip
-chip=${chip:-1}
-if [ $chip -eq 1  ]; then
+EXCLUDE="ai_deck.sh common.sh gap8.sh gap8_v2.sh gapuino_v3.sh"
+EXCLUDE_FILES=
+for i in $EXCLUDE
+do
+    EXCLUDE_FILES="$EXCLUDE_FILES -not -iname *$i"
+done
+
+BOARDS=$(find $GAP_SDK_HOME/configs -maxdepth 1 -type f -iname "*.sh" $EXCLUDE_FILES | sort)
+
+n=0
+board=
+for i in $BOARDS
+do
+    n=$(($n+1))
+    board=$(basename $(echo $BOARDS | cut -d ' ' -f $n))
+    echo -e "\t$n - $board"
+done
+
+read board
+
+if [ -z "$board" ] || [ "$board" -gt "$n" ] || [ "$board" -le 0 ]
+then
     source $GAP_SDK_HOME/configs/gapuino.sh
-elif [ $chip -eq 2  ]; then
-    source $GAP_SDK_HOME/configs/gapuino_v2.sh
-elif [ $chip -eq 3  ]; then
-    source $GAP_SDK_HOME/configs/gapoc_a.sh
-elif [ $chip -eq 4  ]; then
-    source $GAP_SDK_HOME/configs/gapoc_a_v2.sh
-elif [ $chip -eq 5  ]; then
-    source $GAP_SDK_HOME/configs/gapoc_b.sh
-elif [ $chip -eq 6  ]; then
-    source $GAP_SDK_HOME/configs/gapoc_b_v2.sh
 else
-	echo "Unknown board: $chip"
+    board=$(echo $BOARDS | cut -d ' ' -f $board)
+    source $board
 fi
-echo The target board you have chosen is : $BOARD_NAME
+
+echo The target board you have chosen is : $BOARD_NAME, $TARGET_CHIP.
+
+unset EXCLUDE EXCLUDE_FILES BOARDS board n
