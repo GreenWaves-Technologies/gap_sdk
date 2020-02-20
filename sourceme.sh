5a071d6f2e2999ec8cd705bc357db33c32366783
S. Sivarajah <sivarajah.sivapiriyan@greenwaves-technologies.com>
2020-02-20 10:47:02 +0100

    Update sourceme script, add gitignore files to ignore some files from git status

--- sourceme.sh
+++ sourceme.sh
@@ -1,12 +1,45 @@
 #! /bin/bash
 
-export GAP_SDK_HOME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-
-read -p "Which chip you want to use: 1-GAP8 2-GAP8_V2 [1]: " chip
-chip=${chip:-1}
-if [ $chip == "1"  ]; then
-    source $GAP_SDK_HOME/configs/gap8.sh
-elif [ $chip == "2"  ]; then
-    source $GAP_SDK_HOME/configs/gap8_v2.sh
+if [ -n "${ZSH_VERSION:-}" ]
+then
+    DIR="$(readlink -f -- "${(%):-%N}")"
+    DIRNAME="$(dirname $DIR)"
+    GAP_SDK_HOME=$(echo $DIRNAME)
+    export GAP_SDK_HOME
+    setopt shwordsplit
+else
+    export GAP_SDK_HOME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 fi
-echo The target chip you have chosen is : $TARGET_CHIP
+
+EXCLUDE="ai_deck.sh common.sh gap8.sh gap8_v2.sh gapuino_v3.sh"
+EXCLUDE_FILES=
+for i in $EXCLUDE
+do
+    EXCLUDE_FILES="$EXCLUDE_FILES -not -iname *$i"
+done
+
+BOARDS=$(find $GAP_SDK_HOME/configs -maxdepth 1 -type f -iname "*.sh" $EXCLUDE_FILES | sort)
+
+n=0
+board=
+echo "Select the target : "
+for i in $BOARDS
+do
+    n=$(($n+1))
+    board=$(basename $(echo $BOARDS | tr '[:lower:]' '[:upper:]' | cut -d ' ' -f $n) .SH)
+    echo -e "\t$n - $board"
+done
+
+read board
+
+if [ -z "$board" ] || [ "$board" -gt "$n" ] || [ "$board" -le 0 ]
+then
+    source $GAP_SDK_HOME/configs/gapuino.sh
+else
+    board=$(echo $BOARDS | cut -d ' ' -f $board)
+    source $board
+fi
+
+echo The target board you have chosen is : $BOARD_NAME, $TARGET_CHIP.
+
+unset EXCLUDE EXCLUDE_FILES BOARDS board n
