8c7de7ce09682fce4078c19c135fa1812d4f028c
Yaooooo <yao.zhang@greenwaves-technologies.com>
2020-03-22 06:33:03 -0700

    fix the bug of using L3 in Cifar10 example

--- examples/autotiler/Cifar10/Cifar10Model.c
+++ examples/autotiler/Cifar10/Cifar10Model.c
@@ -23,18 +23,47 @@ void Cifar10Model(unsigned int L1Memory)
     SetUsedFilesNames(0, 2, "pmsis.h", "CNN_BasicKernels.h");
     SetGeneratedFilesNames("Cifar10Kernels.c", "Cifar10Kernels.h");
 
-    SetL1MemorySize(L1Memory);
+    //SetL1MemorySize(L1Memory);
+
+    int L3Memory=8*1024*1024;
+
+    SetMemoryDeviceInfos(1,
+        AT_MEM_L3_HRAM, L3Memory, "Cifar10_L3_Memory", 0, 1
+    );
+
     LoadCNNLibrary();
 
     // 5x5 Convolution followed by 2x2 Max pooling. Pure SW.
     // 1 input plane [32x32], 8 output planes [14x14]
-    CNN_ConvolutionPoolReLU("Conv5x5MaxPool2x2_SW_0", 0, 2, 2, 2, 2, 0, 0, 0, 0, 1, 8, 32, 32, KOP_CONV, 5, 5, 1, 1, 1, 1, 0, KOP_MAXPOOL, 2, 2, 1, 1, 2, 2, 1, KOP_NONE);
+    CNN_ConvolutionPoolReLU("Conv5x5MaxPool2x2_SW_0", 0, 2, 2, 2, 2,
+#ifdef COEF_L2
+            0, 0, 0, 0,
+#else
+            0, 1, 1, 1,     // Filter and Bias from L3, output in L3
+#endif
+            1, 8, 32, 32, KOP_CONV, 5, 5,
+            1, 1, 1, 1, 0, KOP_MAXPOOL, 2, 2,
+            1, 1, 2, 2, 1, KOP_NONE);
     // 5x5 Convolution followed by 2x2 Max pooling. Pure SW.
     // 8 input planes [14x14], 12 output planes [5x5]
-    CNN_ConvolutionPoolReLU("Conv5x5MaxPool2x2_SW_1", 0, 2, 2, 2, 2, 0, 0, 0, 0, 8, 12, 14, 14, KOP_CONV, 5, 5, 1, 1, 1, 1, 0, KOP_MAXPOOL, 2, 2, 1, 1, 2, 2, 1, KOP_NONE);
+    CNN_ConvolutionPoolReLU("Conv5x5MaxPool2x2_SW_1", 0, 2, 2, 2, 2,
+#ifdef COEF_L2
+            0, 0, 0, 0,
+#else
+            1, 1, 1, 0,     // Filter and Bias from L3, input from L3
+#endif
+            8, 12, 14, 14, KOP_CONV, 5, 5,
+            1, 1, 1, 1, 0, KOP_MAXPOOL, 2, 2,
+            1, 1, 2, 2, 1, KOP_NONE);
     // Linear Layer
     // Input 12 x [5x5], Output 10
-    CNN_LinearReLU("LinearLayerReLU_1", 0, 2, 2, 2, 2, 0, 0, 0, 0, 12*5*5, 10, KOP_LINEAR, KOP_NONE);
+    CNN_LinearReLU("LinearLayerReLU_1", 0, 2, 2, 2, 2,
+#ifdef COEF_L2
+            0, 0, 0, 0,
+#else
+            0, 1, 1, 0,     // Filter and Bias from L3
+#endif
+            12*5*5, 10, KOP_LINEAR, KOP_NONE);
 }
 
 int main(int argc, char **argv)
