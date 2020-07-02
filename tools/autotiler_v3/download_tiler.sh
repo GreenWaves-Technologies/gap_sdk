d5aaad7dc09e549aeac3e73400a41e75fd966a92
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2020-07-02 09:35:00 +0200

    Updated modules

--- tools/autotiler_v3/download_tiler.sh
+++ tools/autotiler_v3/download_tiler.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-echo "Please read carefully the autotiler agreement before proceeding with the download."
-read -n 1 -s -r -p "Press enter when you are ready to display the license."
-
-more LICENSE
-
-while [ true ] ; do
-read -n 1 -p "Do you agree with the license (y/n) ? " key
-
-echo
-
-if [ "$key" == "y" ]
+if [ -z "$TILER_LICENSE_AGREED" ]
 then
-break
+	echo "Please read carefully the autotiler agreement before proceeding with the download."
+	read -n 1 -s -r -p "Press enter when you are ready to display the license."
+
+	more LICENSE
+
+	while [ true ] ; do
+	read -n 1 -p "Do you agree with the license (y/n) ? " key
+
+	echo
+
+	if [ "$key" == "y" ]
+	then
+	break
+	fi
+
+	if [ "$key" == "n" ]
+	then
+	exit 0
+	fi
+
+	done
 fi
-
-if [ "$key" == "n" ]
-then
-exit 0
-fi
-
-
-done
 
 echo ${TILER_LIB} | wget --no-use-server-timestamps --base=`cat $TILER_URL` --input-file=- -O lib/libtile.a
 if [ $? != 0 ]; then
